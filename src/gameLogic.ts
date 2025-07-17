215b2d6116b6e7b005e07b0e1d6a189c6ea60afb
StenMajor <stenmajor1988@gmail.com>
2025-07-17 13:25:36 +0200

    Update gameLogic.ts

--- src/gameLogic.ts
+++ src/gameLogic.ts
@@ -3,7 +3,7 @@ import { Level } from './types';
 export const levels: Level[] = [
   {
     id: 1,
-    imageSrc: '/FindTheTracktor/images/lv1.png',
+    imageSrc: 'images/lv1.png',
     tractorBoundingBox: {
       x: 26.67,
       y: 29.16,
@@ -13,7 +13,7 @@ export const levels: Level[] = [
   },
   {
     id: 2,
-    imageSrc: '/FindTheTracktor/images/lv2.png',
+    imageSrc: 'images/lv2.png',
     tractorBoundingBox: {
       x: 23.67,
       y: 24.93,
@@ -23,7 +23,7 @@ export const levels: Level[] = [
   },
   {
     id: 3,
-    imageSrc: '/FindTheTracktor/images/lv3.png',
+    imageSrc: 'images/lv3.png',
     tractorBoundingBox: {
       x: 69.33,
       y: 4.49,
