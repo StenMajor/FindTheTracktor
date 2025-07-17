960f3c2f860fd1540d5239385d4f47798da42b04
StenMajor <stenmajor1988@gmail.com>
2025-07-17 12:44:23 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/FindTheTracktor/",   // 👈 wichtig für GitHub Pages
-  root: 'src',                 // 👈 dein Projekt startet in src/
+  base: "/FindTheTracktor/",
   plugins: [react()],
   build: {
-    outDir: '../dist',         // 👈 das Output-Verzeichnis (außerhalb src)
+    outDir: 'dist',
     emptyOutDir: true
   }
-})
+});
